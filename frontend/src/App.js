e1ef8bd31bd154dcb5ab961cf06a22f790edf9af
mizelleaustin <mizelleaustin@gmail.com>
2025-03-19 12:49:33 -0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import StockPortfolio from "./components/StockPortfolio";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI Portfolio Manager</h1>
+      <StockPortfolio />
+    </div>
+  );
+}
+
+export default App;
